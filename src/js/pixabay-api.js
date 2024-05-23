fb91bab871ff359ee71099974c08567f65e7dc94
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2024-05-23 16:00:38 +0300

    1.2

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,20 +1,23 @@
+import axios from 'axios';
+
 const API_KEY = '43967631-6112ded5daea5d7f1facaa737';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export const fetchPhotosByQuery = (q = 'pug') => {
+export const fetchPhotosByQuery = async (q = 'pug', page = 1, per_page = 15) => {
     const searchParams = new URLSearchParams({
         key: API_KEY,
         q,
-        image_type: 'photo',         
-        orientation: 'horizontal',   
+        image_type: 'photo',
+        orientation: 'horizontal',
         safesearch: true,
+        page,
+        per_page,
     });
 
-    return fetch(`${BASE_URL}?${searchParams}`).then(response => {
-        if (!response.ok) {
-            throw new Error(response.statusText);
-        }
-
-        return response.json();
-    });
+    try {
+        const response = await axios.get(`${BASE_URL}?${searchParams}`);
+        return response.data;
+    } catch (error) {
+        throw new Error(error.response ? error.response.statusText : error.message);
+    }
 };
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,36 +1,33 @@
-
 export const createMarkupItem = images => {
   return images
-    .map(
-      ({
-        webformatURL,
-        largeImageURL,
-        tags,
-        likes,
-        views,
-        comments,
-        downloads,
-      }) => {
-        return `
-  <li class="gallery-item">
-    <a class="gallery-link" href="${largeImageURL}">
-      <img
-        class="gallery-image"
-        src="${webformatURL}"
-        alt="${tags}"
-      />
-    </a>
-    <div class="small-content">
-        <span class="text-body-likes"><strong>Likes:</strong> ${likes}</span>
-        <span class="text-body-views"><strong>Views:</strong> ${views}</span>
-        <span class="text-body-comments"><strong>Comments:</strong> ${comments}</span>
-        <span class="text-body-downloads"><strong>Downloads:</strong> ${downloads}</span>
-    </div>
-  </li>
-`;
-      }
-    )
+    .map(image => {
+      const {
+        webformatURL = '',
+        largeImageURL = '',
+        tags = '',
+        likes = 0,
+        views = 0,
+        comments = 0,
+        downloads = 0,
+      } = image;
+
+      return `
+        <li class="gallery-item">
+          <a class="gallery-link" href="${largeImageURL}">
+            <img
+              class="gallery-image"
+              src="${webformatURL}"
+              alt="${tags}"
+            />
+          </a>
+          <div class="small-content">
+            <span class="text-body-likes"><strong>Likes:</strong> ${likes}</span>
+            <span class="text-body-views"><strong>Views:</strong> ${views}</span>
+            <span class="text-body-comments"><strong>Comments:</strong> ${comments}</span>
+            <span class="text-body-downloads"><strong>Downloads:</strong> ${downloads}</span>
+          </div>
+        </li>
+      `;
+    })
     .join('');
 };
-
-
